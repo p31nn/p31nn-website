866da920ed052c4e2c251d7dfcd93cbdd969b481
P31NN <p31nn.ph@gmail.com>
2024-07-30 20:29:52 +0700

    adding flipping word to h1

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -8,7 +8,7 @@ import { ArrowRight } from "lucide-react";
 
 // Local component imports
 import { Section, Container } from "@/components/craft";
-import { Button } from "../ui/button";
+import { FlipWords } from "../ui/flip-words";
 
 
 const Hero = () => {
@@ -18,19 +18,27 @@ const Hero = () => {
         <div>
           <h1>
             <Balancer>
-              Hi, I&apos;m Peam 👋🏻
+              Hi👋🏻, I&apos;m <FlipWords words={["Peam", "P31NN"]}/>
             </Balancer>
           </h1>
+
           <h3 className="text-muted-foreground">
             <Balancer>
-            💊Pharmacy Student | Web Development Enthusiast | AI & Financial Literacy Explorer
+              💊Pharmacy Student | Web Development Enthusiast | AI & Financial Literacy Explorer
             </Balancer>
           </h3>
-          <p>Welcome to my personal space! I’m Peam, a dedicated pharmacy student with a passion for web development, large language models (LLMs), and financial literacy. Dive into my journey as I blend the world of healthcare with the ever-evolving fields of technology and finance.</p>
+
+          <p>
+          <Balancer>
+            Welcome to my personal space! I’m Peam, a dedicated pharmacy student with a passion for web development, large language models (LLMs), and financial literacy. Dive into my journey as I blend the world of healthcare with the ever-evolving fields of technology and finance.
+          </Balancer>
+          </p>
         </div>
       </Container>
     </Section>
   );
 };
+
+
 
 export default Hero;
