261e5db337dec97b42f9160ff0c88a038108f185
P31NN <p31nn.ph@gmail.com>
2024-08-12 20:30:00 +0700

    fix &apos;

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
           <br></br>
           <h1 className="text-primary">Hi, I&apos;m Peam 👋🏻</h1>
           <h3>💊Pharmacy Student | Web Development Enthusiast | AI & Financial Literacy Explorer</h3>
-          <p className="text-muted-foreground">I'm Peam a dedicated pharmacy student with a passion for web development, large language models (LLMs), and financial literacy. Dive into my journey as I blend the world of healthcare with the ever-evolving fields of technology and finance.</p>
+          <p className="text-muted-foreground">I&apos;m Peam a dedicated pharmacy student with a passion for web development, large language models (LLMs), and financial literacy. Dive into my journey as I blend the world of healthcare with the ever-evolving fields of technology and finance.</p>
         </Container>
       </Section>
     </AuroraBackground>
