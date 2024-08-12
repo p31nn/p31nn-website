474cc0d463fdd0f6446d675e3ab55df5ba4e6343
P31NN <p31nn.ph@gmail.com>
2024-08-12 13:43:32 +0700

    Fixing npm run dev automatically shutdown

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,17 +1,18 @@
 import { Section, Container, Article } from "@/components/craft";
+import { AuroraBackground } from "@/components/ui/aurora-background";
 import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ui/mode-toggle";
 
 export default function Home() {
   return (
-    <Section>
-      <Container>
-        <h1 className="text-slate-800 font-black">Hi, I&apos;m Peam 👋🏻</h1>
-        <h3 className="text-slate-500">💊Pharmacy Student | Web Development Enthusiast | AI & Financial Literacy Explorer</h3>
-        <p className="text-slate-600">Welcome to my personal space! I’m Peam, a dedicated pharmacy student with a passion for web development, large language models (LLMs), and financial literacy. Dive into my journey as I blend the world of healthcare with the ever-evolving fields of technology and finance.</p>
-      </Container>
-      <Container>
-        <Button>Get to know me</Button>
-      </Container>
-    </Section>
+    <AuroraBackground>
+      <Section>
+        <Container>
+          <h1 className="text-primary">Hi, I&apos;m Peam 👋🏻</h1>
+          <h3>💊Pharmacy Student | Web Development Enthusiast | AI & Financial Literacy Explorer</h3>
+          <p className="text-muted-foreground">Welcome to my personal space! I’m Peam, a dedicated pharmacy student with a passion for web development, large language models (LLMs), and financial literacy. Dive into my journey as I blend the world of healthcare with the ever-evolving fields of technology and finance.</p>
+        </Container>
+      </Section>
+    </AuroraBackground>
   );
 }
